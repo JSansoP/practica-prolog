6e2c94798cea6711f9c80c6cea16aa3581532ece
Julián Wallis <wallismedina@gmail.com>
2022-06-09 22:12:14 +0200

    optimizacions

--- practica.pl
+++ practica.pl
@@ -106,92 +106,89 @@ mostra([X|Y], F, C, vertical):-
 % 3. Ens asseguram que no estigui repetida.
 % 4. Ens asseguram que cumpleix les restriccions d'encreuament amb les altres
 %    paraules.
+%
+% Per optimitzar el programa només comprovam que no estiguin repetides les
+% paraules de la mateixa mida, ja que una paraula de 5 lletres mai serà la
+% mateixa que una de 6.
 % Un pic les tenim totes les mostram per pantalla.
 creuats:-
   cls,
   paraula(PH1),
-  length(PH1, 7),
+  length(PH1,7),
 
   paraula(PV1),
   length(PV1,11),
-  not(repetides([PH1, PV1])),
   nth1(5,PH1,A),
   nth1(1,PV1,A),
 
   paraula(PH2),
   length(PH2,5),
-  not(repetides([PH1, PH2, PV1])),
   nth1(3,PV1,B),
   nth1(3,PH2,B),
 
   paraula(PH3),
   length(PH3,7),
-  not(repetides([PH1, PH2, PH3, PV1])),
+  not(repetides([PH1, PH3])),
   nth1(5,PH3,C),
   nth1(11,PV1,C),
 
   paraula(PV2),
   length(PV2,3),
-  not(repetides([PH1, PH2, PH3, PV1, PV2])),
   nth1(7,PH3,D),
   nth1(3,PV2,D),
 
   paraula(PV3),
   length(PV3,8),
-  not(repetides([PH1, PH2, PH3, PV1, PV2, PV3])),
   nth1(6,PV3,E),
   nth1(3,PH3,E),
 
   paraula(PH4),
   length(PH4,10),
-  not(repetides([PH1, PH2, PH3, PH4, PV1, PV2, PV3])),
   nth1(6,PH4,F),
   nth1(8,PV3,F),
 
   paraula(PH5),
   length(PH5,8),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PV1, PV2, PV3])),
+  not(repetides([PV3, PH5])),
   nth1(8,PH5,G),
   nth1(1,PV3,G),
 
   paraula(PV4),
   length(PV4,6),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PV1, PV2, PV3, PV4])),
   nth1(4,PV4,H),
   nth1(6,PH5,H),
 
-
   paraula(PV5),
   length(PV5,8),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PV1, PV2, PV3, PV4, PV5])),
+  not(repetides([PH5, PV3, PV5])),
   nth1(5,PV5,I),
   nth1(4,PH5,I),
 
 
   paraula(PV6),
   length(PV6,8),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PV1, PV2, PV3, PV4, PV5, PV6])),
+  not(repetides([PH5, PV3, PV5, PV6])),
   nth1(1,PV6,J),
   nth1(1,PH5,J),
 
 
   paraula(PH6),
   length(PH6,6),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PH6, PV1, PV2, PV3, PV4, PV5, PV6])),
+  not(repetides([PH6, PV4])),
   nth1(6,PH6,K),
   nth1(3,PV5,K),
 
 
   paraula(PH7),
   length(PH7,5),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PH6, PH7, PV1, PV2, PV3, PV4, PV5, PV6])),
+  not(repetides([PH2, PH7])),
   nth1(2,PH7,L),
   nth1(6,PV6,L),
 
 
   paraula(PV7),
   length(PV7,5),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PH6, PH7, PV1, PV2, PV3, PV4, PV5, PV6, PV7])),
+  not(repetides([PH2, PH7, PV7])),
   nth1(1,PV7,M),
   nth1(1,PH6,M),
 
@@ -235,87 +232,81 @@ creuats:-
 creuats2:-
   cls,
   paraula2(PH1),
-  length(PH1, 7),
+  length(PH1,7),
 
   paraula2(PV1),
   length(PV1,11),
-  not(repetides([PH1, PV1])),
   nth1(5,PH1,A),
   nth1(1,PV1,A),
 
   paraula2(PH2),
   length(PH2,5),
-  not(repetides([PH1, PH2, PV1])),
   nth1(3,PV1,B),
   nth1(3,PH2,B),
 
   paraula2(PH3),
   length(PH3,7),
-  not(repetides([PH1, PH2, PH3, PV1])),
+  not(repetides([PH1, PH3])),
   nth1(5,PH3,C),
   nth1(11,PV1,C),
 
   paraula2(PV2),
   length(PV2,3),
-  not(repetides([PH1, PH2, PH3, PV1, PV2])),
   nth1(7,PH3,D),
   nth1(3,PV2,D),
 
   paraula2(PV3),
   length(PV3,8),
-  not(repetides([PH1, PH2, PH3, PV1, PV2, PV3])),
   nth1(6,PV3,E),
   nth1(3,PH3,E),
 
   paraula2(PH4),
   length(PH4,10),
-  not(repetides([PH1, PH2, PH3, PH4, PV1, PV2, PV3])),
   nth1(6,PH4,F),
   nth1(8,PV3,F),
 
   paraula2(PH5),
   length(PH5,8),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PV1, PV2, PV3])),
+  not(repetides([PV3, PH5])),
   nth1(8,PH5,G),
   nth1(1,PV3,G),
 
   paraula2(PV4),
   length(PV4,6),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PV1, PV2, PV3, PV4])),
   nth1(4,PV4,H),
   nth1(6,PH5,H),
 
   paraula2(PV5),
   length(PV5,8),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PV1, PV2, PV3, PV4, PV5])),
+  not(repetides([PH5, PV3, PV5])),
   nth1(5,PV5,I),
   nth1(4,PH5,I),
 
 
   paraula2(PV6),
   length(PV6,8),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PV1, PV2, PV3, PV4, PV5, PV6])),
+  not(repetides([PH5, PV3, PV5, PV6])),
   nth1(1,PV6,J),
   nth1(1,PH5,J),
 
 
   paraula2(PH6),
   length(PH6,6),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PH6, PV1, PV2, PV3, PV4, PV5, PV6])),
+  not(repetides([PH6, PV4])),
   nth1(6,PH6,K),
   nth1(3,PV5,K),
 
 
   paraula2(PH7),
   length(PH7,5),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PH6, PH7, PV1, PV2, PV3, PV4, PV5, PV6])),
+  not(repetides([PH2, PH7])),
   nth1(2,PH7,L),
   nth1(6,PV6,L),
 
 
   paraula2(PV7),
   length(PV7,5),
-  not(repetides([PH1, PH2, PH3, PH4, PH5, PH6, PH7, PV1, PV2, PV3, PV4, PV5, PV6, PV7])),
+  not(repetides([PH2, PH7, PV7])),
   nth1(1,PV7,M),
   nth1(1,PH6,M),
 
